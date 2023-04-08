fdf5651f48e874d189dd2baf0d9b95d61e9d1d3b
Zhanhu Sun <zhanhu.sun.cn@gmail.com>
2023-04-08 23:48:04 +0800

    Fix code

--- models/purchaseOrders.js
+++ models/purchaseOrders.js
@@ -13,86 +13,86 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       code: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false
       },
       name: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false
       },
       consumptionTax: {
-        type: Sequelize.DataTypes.DECIMAL,
+        type: DataTypes.DECIMAL,
         defaultValue: 0,
         allowNull: false
       },
       transportation: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: true
       },
       commission: {
-        type: Sequelize.DataTypes.DECIMAL,
+        type: DataTypes.DECIMAL,
         defaultValue: 0,
         allowNull: false
       },
       status: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: true
       },
       createdBy: {
-        type: Sequelize.DataTypes.UUID,
+        type: DataTypes.UUID,
         allowNull: true
       },
       createdByName: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: true
       },
       createdAt: {
-        type: Sequelize.DataTypes.DATE,
+        type: DataTypes.DATE,
         allowNull: false
       },
       updatedBy: {
-        type: Sequelize.DataTypes.UUID,
+        type: DataTypes.UUID,
         allowNull: true
       },
       updatedByName: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: true
       },
       updatedAt: {
-        type: Sequelize.DataTypes.DATE,
+        type: DataTypes.DATE,
         allowNull: false
       },
       startDate: {
-        type: Sequelize.DataTypes.DATE,
+        type: DataTypes.DATE,
         allowNull: false
       },
       deleted: {
-        type: Sequelize.DataTypes.BOOLEAN,
+        type: DataTypes.BOOLEAN,
         defaultValue: false,
         allowNull: false
       },
       ownerId: {
-        type: Sequelize.DataTypes.UUID,
+        type: DataTypes.UUID,
         allowNull: true
       },
       ownerName: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: true
       },
       clause: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: true
       },
       description: {
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: true
       },
       createdAddress: {
-        type: Sequelize.DataTypes.JSONB,
+        type: DataTypes.JSONB,
         allowNull: true
       },
       sendAddress: {
-        type: Sequelize.DataTypes.JSONB,
+        type: DataTypes.JSONB,
         allowNull: true
       }
     },
