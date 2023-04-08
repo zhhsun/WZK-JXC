fa9cac48aa785a2a4cfd6acc5651edf57c109a84
Zhanhu Sun <zhanhu.sun.cn@gmail.com>
2023-04-08 23:46:30 +0800

    Add po service and controller

---
+++ controllers/purchaseOrderCotroller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class PurchaseOrdersController extends BaseController {
+  /**
+   * 
+   */
+  constructor() {}
+}
+
+module.exports = new PurchaseOrdersController();
---
+++ services/purchaseOrderService.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class PurchaseOrderService {
+  /**
+   * 
+   */
+  constructor () {}
+
+  async createOrder(logger, order) {
+    return {};
+  }
+}
+
+module.exports = new PurchaseOrderService();
