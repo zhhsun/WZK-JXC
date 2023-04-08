04b4aa82061b4fae085a2e7ddd348650dd39af3b
Zhanhu Sun <zhanhu.sun.cn@gmail.com>
2023-04-08 18:09:48 +0800

    initial commit

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const creds = {
+  development: {
+    username: 'sunk',
+    password: '',
+    database: 'sunk',
+    host: '127.0.0.1',
+    dialect: 'postgresql'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgresql'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgresql'
+  }
+};
+
+module.exports = creds;
