1eac1c830e9c95842a118b04521d25f23fdfdb18
Zhanhu Sun <zhanhu.sun.cn@gmail.com>
2023-04-09 14:56:10 +0800

    remove ejs

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 const classroomController = require('../controllers').classroom;
-const { purchasedOrder } = require('../controllers')
+const { purchaseOrder } = require('../controllers')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -18,6 +18,6 @@ router.post('/api/classroom', classroomController.add);
 router.put('/api/classroom/:id', classroomController.update);
 router.delete('/api/classroom/:id', classroomController.delete);
 
-router.post('/api/v1/companies/:company-id/purchase-orders', purchasedOrder.prototype.create);
+router.post('/api/v1/companies/:company-id/purchase-orders', purchaseOrder.prototype.create);
 
 module.exports = router;
